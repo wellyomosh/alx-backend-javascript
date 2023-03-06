4b8754453e0789c8eaaf97ae2b0a04ccd04d3320
wellyomosh <wellyomosh@gmail.com>
2023-03-06 12:43:28 +0000

    My javascript classes

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,20 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    //getter method for sqft
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    //getter method for floor;
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    //Overrode evacuationWarningMessage
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
